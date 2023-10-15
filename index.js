ee59abaedd9ecb93fdbfdc6a7152426e05c1c15b
“ShwetaD01” <shwetadonode819@gmail.com>
2023-10-15 13:29:44 +0530

    Server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const cors = require('cors'); // Correct the import statement
+
+const app = express();
+
+app.use(cors({ origin: 'http://localhost:3000' }));
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log('server connected');
+});
+
+httpServer.listen(5000);
