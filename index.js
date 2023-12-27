2dd5b4ff37da754c6f5f4366827601b70076fa55
ShwetaD01 <shwetadonode819@gmail.com>
2023-12-27 21:43:57 +0530

    server

--- index.js
+++ index.js
@@ -4,16 +4,28 @@ const { Server } = require("socket.io");
 const cors = require('cors'); // Correct the import statement
 
 const app = express();
-
-app.use(cors({ origin: 'http://localhost:3000' }));
+const isDev = app.settings.env === 'development'
+const URL = isDev ? 'http://localhost:3000' : 'https://main--curious-marzipan-421d93.netlify.app'
+app.use(cors({ origin: URL }));
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors:  "http://localhost:3000"
+  cors:  URL
 });
 
-io.on("connection", (socket) => {
+io.on("connect", (socket) => {
   console.log('server connected');
+
+
+  socket.on('designPath',(arg)=>{
+    socket.broadcast.emit('designPath', arg)
+  })
+  socket.on('drawLine',(arg)=>{
+    socket.broadcast.emit('drawLine', arg)
+  })
+  socket.on('changeConfig',(arg)=>{
+    socket.broadcast.emit('changeConfig', arg)
+  })
 });
 
 httpServer.listen(5000);
